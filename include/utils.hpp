9a5a723fcc3b87a156ee1e643cfcaad1185a3598
0xAAAAA <azb.0x7dc@gmail.com>
2019-12-30 21:37:25 +0200

    Move utils to separate header; add tests

---
+++ include/utils.hpp
@@ -0,0 +1,21 @@
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> splitLine(const std::string& line, char delimiter) {
+    std::vector<std::string> result {};
+    std::stringstream ss {line};
+    std::string item;
+    
+    while(std::getline(ss, item, delimiter)) {
+        result.push_back(item);
+    }
+    return result;
+}
+
+
+bool isPrefix(const std::string& str, const std::string& prefix) {
+    if (str.size() > prefix.size()) return false;
+    return std::equal(str.begin(), str.end(), prefix.begin());
+
+}
---
+++ tests/test_utils.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "utils.hpp"
+
+
+TEST_SUITE("test utils") {
+    TEST_CASE("test line split") {
+        std::string line = "Linux\\Debugger|TestC@se";
+        REQUIRE_EQ(std::vector<std::string> {"Linux\\Debugger", "TestC@se"},
+                  splitLine(line, '|'));
+        REQUIRE_NE(std::vector<std::string> {"Linux", "Debugger", "TestC@se"},
+                  splitLine(line, '\\'));
+    }
+    TEST_CASE("test prefix") {
+        std::string line = "command run";
+        REQUIRE_EQ(true, isPrefix("command ", line));
+        REQUIRE_NE(true, isPrefix(line, "command "));
+    }
+}
